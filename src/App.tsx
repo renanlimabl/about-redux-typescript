fca9a7e00aea32c1c76e57e2134fad831e6133b3
Renan Lima <renanlimabl@gmail.com>
2021-01-22 16:26:24 -0300

    add components catalog

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import React from 'react';
  englobamos tudo que precisamos que herde da nossa store. 
  */
 import { Provider } from 'react-redux';
+import Catalog from './components/Catalog';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>Hello World</h1>
+      <Catalog />
     </Provider>
   );
 }
---
+++ src/components/Catalog.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Catalog: React.FC = () => {
+  return (
+    <h1>Catalog</h1>
+  )
+}
+
+export default Catalog;
