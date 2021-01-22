b17abe1637a9d650c4b089721a2ccd06c0c310b4
Renan Lima <renanlimabl@gmail.com>
2021-01-22 19:58:20 -0300

    add api e consome no catalog

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // useStore é um hook para acessar o nosso store, porém ele retorna muita coisa.
 // O melhor para acessar um estado do redux é useSelector
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
+import api from '../services/api';
+import { IProduct } from '../store/modules/cart/types';
 
 const Catalog: React.FC = () => {
   /**
@@ -9,12 +11,27 @@ const Catalog: React.FC = () => {
    * o nosso state, o qual podemos acessar cada propriedade desse state, por ex:
    * const email = useSelector(state => state.email)
    */
-  const catalog = useSelector(state => state);
+  // const catalog = useSelector(state => state);
 
-  console.log(catalog)
+  const [catalog, setCatalog] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
 
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.price}</strong> {" - "}
+          <span>{product.price}</span> {" - "}
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
   )
 }
 
