648709d854890a3780bdfcd87fabaa5ce0e99089
Renan Lima <renanlimabl@gmail.com>
2021-01-22 16:35:15 -0300

    add useSelector with a object from redux store

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+// useStore é um hook para acessar o nosso store, porém ele retorna muita coisa.
+// O melhor para acessar um estado do redux é useSelector
+import { useSelector } from 'react-redux';
 
 const Catalog: React.FC = () => {
+  /**
+   * useSelector recebe uma função como argumento, que podemos acessar diretamente
+   * o nosso state, o qual podemos acessar cada propriedade desse state, por ex:
+   * const email = useSelector(state => state.email)
+   */
+  const state = useSelector(state => state);
+
+  console.log(state)
+
   return (
     <h1>Catalog</h1>
   )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,12 @@
 // createStore é chamada apenas uma vez na aplicação, ela quem vai criar nossa store.
 import { createStore } from 'redux';
 
-const store = createStore(() => []);
+const store = createStore(() => {
+  return {
+    id: 7,
+    name: 'Renan',
+    email: 'renanlimabl@gmail.com'
+  }
+});
 
 export default store;
