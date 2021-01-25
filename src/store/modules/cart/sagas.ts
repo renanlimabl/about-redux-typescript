580da78e74e5ac1396e7c890f1c29900080f5a70
Renan Lima <renanlimabl@gmail.com>
2021-01-25 05:31:33 -0300

    checa a quantidade atual adicionada

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,14 +1,49 @@
-import { all, takeLatest } from 'redux-saga/effects';
+/**
+ * takeLatest serve para executar apenas o último click do usuário basicamente.
+ * o select serve para ter acesso ao estado.
+ */
+import { all, takeLatest, select } from 'redux-saga/effects';
+import { IState } from '../..';
+import { addProductToCart } from './actions';
 
+/**
+ * Dentro do sagas, sempre utilizaremos um generator
+ */
 // Checaremos se existe ainda estoque do produto, interceptando a action antes de chegar no reducer
-function checkProductStock() {
-  console.log('Adicionou ao carrinho')
+/**
+ * 
+ * Para a gente tipar essa action do checkProductStock(action) é complicado no typescript, 
+ * porém é preciso, então temos que pegar o retorno da action, no caso addProductToCart,
+ * utilizaremos a palavra chave do typescript ReturnType<typeof addProductToCart>
+ */
+type CheckProductRequest = ReturnType<typeof addProductToCart>
+
+/**
+ * Nossa action é a função addProductToCart, que no caso retorna o type e o payload,
+ * então podemos portanto, obter o payload através do action.payload ou então fazer a
+ * destruturação. { payload }
+ */
+function* checkProductStock({ payload }: CheckProductRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    /**
+     * Procuramos a quantidade do produto passado no carrinho,
+     * o "?.quantity" diz que: se achar no método .find() então pega a quantity
+     * e o "??" no typescript fala que se retornar undefined, então o valor padrão da quantity será 0
+     */
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
+  })
+
+  console.log(currentQuantity)
 }
 
 export default all([
   /**
    * Primeiro parâmetro do takeLatest é qual action você quer interceptar.
    * Segundo parâmetro é a função que quero disparar quando essa action for acionada.
+   * E essa função do segundo parâmetro é passada as actions para os parâmetros dela.
+   * Ex: checkProductStock(action)
    */
   takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
 ])
